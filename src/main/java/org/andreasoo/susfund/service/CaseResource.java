ddd0be0ebec8dcebd44637e2ee793d8d8afc7073
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-03-07 19:06:27 +0100

    Fixed mysql dependencies.
    Had to the following in standalone.xml
    
    <datasource jndi-name="java:/jdbc/MySQLDataSource" pool-name="MySQLDS" enabled="true">
       <connection-url>jdbc:mysql://localhost:3306/susfund_db</connection-url>
              <driver>mysql</driver>
     </datasource>
    
     and this:
    
    <driver name="mysql" module="com.mysql">
          <driver-class>com.mysql.cj.jdbc.Driver</driver-class>
    </driver>
    
    Then I had to download platform indepedent connectorJ and add the .jar file in modules/com/mysql/main, along with a module.xml file containing the following:
    
    <module xmlns="urn:jboss:module:1.1" name="com.mysql">
    <resources>
    <resource-root path="mysql-connector-j-9.2.0.jar"/>
    </resources>
    <dependencies>
    <module name="javax.api"/>
    <module name="javax.transaction.api"/>
    </dependencies>
    </module>
    
    Finally I had to add the following in the persitence.xml:
    
    <property name="jakarta.persistence.jdbc.user"   value="root" />
    <property name="jakarta.persistence.jdbc.password" value="SECRET" />

--- src/main/java/org/andreasoo/susfund/service/CaseResource.java
+++ src/main/java/org/andreasoo/susfund/service/CaseResource.java
@@ -1,14 +1,24 @@
 package org.andreasoo.susfund.service;
 
+import jakarta.annotation.Resource;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import org.andreasoo.susfund.entity.Case;
 
+import javax.sql.DataSource;
 import java.util.List;
+
+
 
 @Path("/cases")
 public class CaseResource {
+
+    @PersistenceContext(unitName = "myUnit")
+    private EntityManager entityManager;
+
     @GET
     @Produces("application/json")
     public List<Case> getAllCases() {
