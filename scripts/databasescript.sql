ab804d47b270da2a8e9aa1fa3a871295f2dfbcd5
Josefin Törner <josefintorner@gmail.com>
2025-03-24 14:49:09 +0100

    Added case decision and case decision result tables

--- scripts/databasescript.sql
+++ scripts/databasescript.sql
@@ -38,7 +38,21 @@ DROP TABLE IF EXISTS `case_decision_type`;
 CREATE TABLE `case_decision_type` ( `id` INT NOT NULL AUTO_INCREMENT,
 						`name` VARCHAR(255) NOT NULL,
 						 PRIMARY KEY (`id`)
-					  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;                        
+					  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `case_decision_result`;
+CREATE TABLE `case_decision_result` ( `id` INT NOT NULL AUTO_INCREMENT,
+                                      `name` VARCHAR(255) NOT NULL,
+                                      PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `case_decision`;
+CREATE TABLE `case_decision` ( `id` INT NOT NULL AUTO_INCREMENT,
+                                `case_decision_result_id` INT NOT NULL,
+                                `decision_date` DATE NOT NULL,
+                                CONSTRAINT `FK_case_decision_result_id` FOREIGN KEY  (`case_decision_result_id`) REFERENCES  `case_decision_result` (`id`),
+                                PRIMARY KEY (`id`)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
                 
 DROP TABLE IF EXISTS `cases`;
 CREATE TABLE `cases` ( `id` INT NOT NULL AUTO_INCREMENT,
@@ -47,13 +61,14 @@ CREATE TABLE `cases` ( `id` INT NOT NULL AUTO_INCREMENT,
                         `case_manager_id` INT NOT NULL,
                         `case_status_id` INT NOT NULL,
                         `case_decision_type_id` INT NOT NULL,
+                        `case_decision_id` INT,
                         CONSTRAINT `FK_organization_id` FOREIGN KEY (`organization_id`) REFERENCES `organization` (`id`),
                         CONSTRAINT `FK_case_manager_id` FOREIGN KEY (`case_manager_id`) REFERENCES `case_manager` (`id`),
                         CONSTRAINT `FK_case_status_id` FOREIGN KEY (`case_status_id`) REFERENCES `case_status` (`id`),
                         CONSTRAINT `FK_case_decision_type_id` FOREIGN KEY (`case_decision_type_id`) REFERENCES `case_decision_type` (`id`),
+                        CONSTRAINT `FK_case_decision_id` FOREIGN KEY (`case_decision_id`) REFERENCES `case_decision` (`id`),
 						 PRIMARY KEY (`id`)
-					  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;          
-          
+					  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 INSERT INTO `susfund_db`.`organization_type` (`name`) VALUES ("SOLE_TRADER");
 INSERT INTO `susfund_db`.`organization_type` (`name`) VALUES ("LIMITED_COMPANY"); 
@@ -85,14 +100,22 @@ INSERT INTO `susfund_db`.`case_decision_type` (`name`) VALUES ("Payment request"
 INSERT INTO `susfund_db`.`case_decision_type` (`name`) VALUES ("Repayment");
 INSERT INTO `susfund_db`.`case_decision_type` (`name`) VALUES ("Closed");
 
+INSERT INTO `susfund_db`.`case_decision_result` (`name`) VALUES ("Approved");
+INSERT INTO `susfund_db`.`case_decision_result` (`name`) VALUES ("Rejected");
+INSERT INTO `susfund_db`.`case_decision_result` (`name`) VALUES ("Partially approved");
 
-INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`) VALUES ("Going solar", 1, 1,1,1);
-INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`) VALUES ("I got wind", 1, 1,2,2);
-INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`) VALUES ("Shop locally", 1, 1,3,3);
-INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`) VALUES ("Funding for future", 1, 2,4,4);
-INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`) VALUES ("Going green", 2, 3,2,2);
-INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`) VALUES ("No more gaslighting", 3, 4,2,2); 
-INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`) VALUES ("Making the change", 4, 5,2,2);
+INSERT INTO `susfund_db`.`case_decision` (`case_decision_result_id`, `decision_date`) VALUES (1, "2025-03-23");
+INSERT INTO `susfund_db`.`case_decision` (`case_decision_result_id`, `decision_date`) VALUES (2, "2025-02-14");
+INSERT INTO `susfund_db`.`case_decision` (`case_decision_result_id`, `decision_date`) VALUES (3, "2025-03-10");
+INSERT INTO `susfund_db`.`case_decision` (`case_decision_result_id`, `decision_date`) VALUES (1, "2025-03-20");
+
+INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`, `case_decision_id`) VALUES ("Going solar", 1, 1,1,1, 1);
+INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`, `case_decision_id`) VALUES ("I got wind", 1, 1,2,2, 2);
+INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`, `case_decision_id`) VALUES ("Shop locally", 1, 1,3,3, 3);
+INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`, `case_decision_id`) VALUES ("Funding for future", 1, 2,4,4, 4);
+INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`, `case_decision_id`) VALUES ("Going green", 2, 3,2,2, null);
+INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`, `case_decision_id`) VALUES ("No more gaslighting", 3, 4,2,2, null);
+INSERT INTO `susfund_db`.`cases` (`name`, `organization_id`, `case_manager_id`, `case_status_id`, `case_decision_type_id`, `case_decision_id`) VALUES ("Making the change", 4, 5,2,2, null);
 
                       
                          
