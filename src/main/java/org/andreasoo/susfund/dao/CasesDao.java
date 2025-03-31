ef600678dc17466812d1447bd14233858645c2e0
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-03-31 21:46:18 +0200

    Added daos and injection in service - refactored old methods in service

---
+++ src/main/java/org/andreasoo/susfund/dao/CasesDao.java
@@ -0,0 +1,12 @@
+package org.andreasoo.susfund.dao;
+
+import jakarta.inject.Inject;
+import org.andreasoo.susfund.entity.Cases;
+
+import java.util.List;
+
+
+public interface CasesDao {
+    List<Cases> getAllCases();
+    Cases getCaseById(int id);
+}
--- src/main/java/org/andreasoo/susfund/service/CasesResource.java
+++ src/main/java/org/andreasoo/susfund/service/CasesResource.java
@@ -1,11 +1,16 @@
 package org.andreasoo.susfund.service;
 
+
+import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import org.andreasoo.susfund.dao.CaseAssessmentDao;
+import org.andreasoo.susfund.dao.CasesDao;
+import org.andreasoo.susfund.dao.CasesDaoImpl;
 import org.andreasoo.susfund.entity.AssessmentResult;
 import org.andreasoo.susfund.entity.Cases;
 import org.andreasoo.susfund.util.AssessmentUtil;
@@ -13,41 +18,32 @@ import org.andreasoo.susfund.util.AssessmentUtil;
 import java.util.List;
 
 
+
 @Path("/cases")
 public class CasesResource {
 
-    @PersistenceContext()
-    private EntityManager entityManager;
+    @Inject
+    private CasesDao casesDao;
+
+    @Inject
+    private CaseAssessmentDao caseAssessmentDao;
 
     @GET
     @Produces("application/json")
     public List<Cases> getAllCases() {
-        return entityManager.createNamedQuery("Cases.findAll", Cases.class).getResultList();
+        return casesDao.getAllCases();
     }
     @Path("/{id}")
     @GET()
     @Produces("application/json")
     public Cases getCaseById(@PathParam("id") int id) {
-        return entityManager.find(Cases.class, id);
+        return casesDao.getCaseById(id);
     }
 
     @Path("/{id}/assessment")
     @GET()
     @Produces("application/json")
     public AssessmentUtil getAssessmentUtilByCaseId(@PathParam("id") int id) {
-        Cases cases = entityManager.find(Cases.class, id);
-        AssessmentUtil assessmentUtil = new AssessmentUtil();
-        assessmentUtil.setCaseAssessmentId(cases.getCaseAssessment().getId());
-
-        List<List<AssessmentResult>> result =
-                cases.getCaseAssessment().getAssessmentResults().stream()
-                        .map(res1 -> res1.getAssessmentItem().getAssessmentSection().getId())
-                        .distinct()
-                        .map(sectionId -> cases.getCaseAssessment().getAssessmentResults().stream()
-                                                        .filter(res2 -> res2.getAssessmentItem().getAssessmentSection().getId() == sectionId)
-                                                        .toList())
-                .toList();
-        assessmentUtil.setAssessmentResults(result);
-        return assessmentUtil;
+        return caseAssessmentDao.getAssessmentUtilByCaseId(id);
     }
 }
