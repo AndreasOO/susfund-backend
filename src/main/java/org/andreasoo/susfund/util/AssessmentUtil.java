411cc6f95055a81190b5a7d9f4c9fe85651aaac3
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-03-31 14:45:49 +0200

    Added endpoint in case service and util class for case assessment

---
+++ src/main/java/org/andreasoo/susfund/util/AssessmentUtil.java
@@ -0,0 +1,30 @@
+package org.andreasoo.susfund.util;
+
+import org.andreasoo.susfund.entity.AssessmentResult;
+
+
+import java.util.List;
+
+public class AssessmentUtil {
+    private int caseAssessmentId;
+    private List<List<AssessmentResult>> assessmentResults;
+
+    public AssessmentUtil() {
+    }
+
+    public Integer getCaseAssessmentId() {
+        return caseAssessmentId;
+    }
+
+    public void setCaseAssessmentId(int caseAssessmentId) {
+        this.caseAssessmentId = caseAssessmentId;
+    }
+
+    public List<List<AssessmentResult>> getAssessmentResults() {
+        return assessmentResults;
+    }
+
+    public void setAssessmentResults(List<List<AssessmentResult>> assessmentResults) {
+        this.assessmentResults = assessmentResults;
+    }
+}
