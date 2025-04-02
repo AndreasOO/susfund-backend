fdc8e6c4808c246c5ce2fa38ba45b7f4a4b7092e
Josefin Törner <josefintorner@gmail.com>
2025-04-02 17:22:30 +0200

    added tables user_credentials, user_password, user_role, user_roles_mapping to databasescript

--- scripts/databasescript.sql
+++ scripts/databasescript.sql
@@ -173,6 +173,39 @@ CREATE TABLE `history_event` ( `id` INT NOT NULL AUTO_INCREMENT,
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 -- -----------------------------------------END VIKTORS HISTORY IMPLEMENTATION
+-- -----------------------------------------START JOSEFINS USER IMPLEMENTATION
+
+
+DROP TABLE IF EXISTS `user_credentials`;
+CREATE TABLE `user_credentials` (`id` INT NOT NULL AUTO_INCREMENT,
+                                `user_name` VARCHAR(255) NOT NULL,
+                                `case_manager_id` INT NOT NULL,
+                                 PRIMARY KEY (`id`),
+                                 CONSTRAINT `FK_case_manager_id2` FOREIGN KEY (`case_manager_id`) REFERENCES `case_manager` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `user_password`;
+CREATE TABLE `user_password` (`id` INT NOT NULL AUTO_INCREMENT,
+                              `password` VARCHAR(255) NOT NULL,
+                              `user_credentials_id` INT NOT NULL,
+                               PRIMARY KEY (`id`),
+                               CONSTRAINT `FK_user_credentials_id` FOREIGN KEY (`user_credentials_id`) REFERENCES `user_credentials` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` ( `id` INT NOT NULL AUTO_INCREMENT,
+                           `name` VARCHAR(255) NOT NULL,
+                            PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `user_roles_mapping`;
+CREATE TABLE `user_roles_mapping` ( `user_credentials_id` INT NOT NULL,
+                            `user_role_id` INT NOT NULL,
+                            CONSTRAINT `FK_user_credentials_id2` FOREIGN KEY (`user_credentials_id`) REFERENCES `user_credentials` (`id`),
+                            CONSTRAINT `FK_user_role_id` FOREIGN KEY (`user_role_id`) REFERENCES `user_role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+-- ------------------------------------END JOSEFINS USER IMPLEMENTATION
 
 INSERT INTO `susfund_db`.`organization_type` (`name`) VALUES ("SOLE_TRADER");
 INSERT INTO `susfund_db`.`organization_type` (`name`) VALUES ("LIMITED_COMPANY"); 
