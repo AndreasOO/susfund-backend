f49f07638d89412a26487b049de6f6dd8c1e2785
Josefin Törner <josefintorner@gmail.com>
2025-04-03 00:11:01 +0200

    imported JJWT dependencies in pom.xml to create tokens

--- src/main/java/org/andreasoo/susfund/service/AuthenticationResource.java
+++ src/main/java/org/andreasoo/susfund/service/AuthenticationResource.java
@@ -1,14 +1,25 @@
 package org.andreasoo.susfund.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
 import org.andreasoo.susfund.entity.UserCredentials;
 import org.andreasoo.susfund.entity.UserPassword;
+
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Path("/auth")
 public class AuthenticationResource {
@@ -16,6 +27,9 @@ public class AuthenticationResource {
     @PersistenceContext
     private EntityManager entityManager;
 
+    SecretKey key = Keys.hmacShaKeyFor("yourSecretKey".getBytes(StandardCharsets.UTF_8));
+
+    // ta in rätt parametrar
     @POST
     @Path("/login")
     public Response login(String username, String password) {
@@ -30,14 +44,28 @@ public class AuthenticationResource {
             user = query.getSingleResult();
         }
         catch (NoResultException e) {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid username or password").build();
         }
 
         // kolla om angivna lösenordet är samma som i user-objektet
         if(!password.equals(user.getUserPassword().getPassword())) {
-            return Response.status(Response.Status.UNAUTHORIZED).build();
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid username or password").build();
         }
 
-        return Response.ok().build();
+
+        // vad ska returneras?
+
+        String token = generateToken(user);
+        return Response.ok().entity("Bearer " + token).build();
+    }
+
+    // Skapa upp token som kan returneras till klienten som gjort anropet, som klienten kan användas framtida requests
+    public String generateToken(UserCredentials user) {
+        return Jwts.builder()
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // expiration 1 timme
+                .signWith(key)
+                .compact();
     }
 }
