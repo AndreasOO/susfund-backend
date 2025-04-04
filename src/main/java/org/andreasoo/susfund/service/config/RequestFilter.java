a50459a857caa641e2642a4d072866182b01d1b7
Josefin Törner <josefintorner@gmail.com>
2025-04-04 16:04:51 +0200

    updated deprecated parser methods when parsing header to jwt

--- src/main/java/org/andreasoo/susfund/service/config/RequestFilter.java
+++ src/main/java/org/andreasoo/susfund/service/config/RequestFilter.java
@@ -1,6 +1,7 @@
 package org.andreasoo.susfund.service.config;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.SignatureException;
 import jakarta.ws.rs.container.ContainerRequestContext;
@@ -8,6 +9,8 @@ import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
+
+import java.nio.charset.StandardCharsets;
 
 @Provider
 public class RequestFilter implements ContainerRequestFilter {
@@ -17,18 +20,35 @@ public class RequestFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) {
-        String token = requestContext.getHeaders().getFirst("Authentication");
+        String authHeader = requestContext.getHeaders().getFirst("Authorization");
+
+        if(authHeader == null ||authHeader.isEmpty()) {
+            Response response = Response.status(Response.Status.UNAUTHORIZED).type(MediaType.TEXT_PLAIN_TYPE).entity("Token header missing").build();
+            requestContext.abortWith(response);
+        }
 
         try{
-            Claims claims = Jwts.parser().setSigningKey(SECRET_KEY.getBytes()).parseClaimsJws(token).getBody();
+
+            // parseClaimsJws försöker tolka token och verifierar signaturen
+            Claims claims = Jwts.parserBuilder().setSigningKey(SECRET_KEY.getBytes(StandardCharsets.UTF_8)).build().parseClaimsJws(authHeader).getBody();
 
             // om man vill kunna läsa av olika properties till ContainerRequestContext-obj i resource-klass behöver man sätta dem från claims-obj här
             requestContext.setProperty("username", claims.getSubject());
             requestContext.setProperty("role", claims.get("role", String.class));
         }
-        catch(SignatureException e){
-            Response response = Response.status(Response.Status.UNAUTHORIZED).type(MediaType.TEXT_PLAIN_TYPE).entity("Token header invalid").build();
-            requestContext.abortWith(response);
+        catch(ExpiredJwtException e){
+            abortWithUnauthorized(requestContext, "Expired token");
         }
+        catch (Exception e){
+            abortWithUnauthorized(requestContext, "Invalid token");
+        }
+    }
+
+    private void abortWithUnauthorized(ContainerRequestContext requestContext, String message) {
+        Response response = Response.status(Response.Status.UNAUTHORIZED)
+                .type(MediaType.TEXT_PLAIN_TYPE)
+                .entity(message)
+                .build();
+        requestContext.abortWith(response);
     }
 }
