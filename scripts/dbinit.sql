204806dcffd41286dc602495f47b7d9297dfd289
Josefin Törner <josefintorner@gmail.com>
2025-04-12 20:15:55 +0200

    checkpoint

--- scripts/dbinit.sql
+++ scripts/dbinit.sql
@@ -1,11 +1,11 @@
-DROP USER IF EXISTS'casemanager'@'localhost';
+DROP USER IF EXISTS 'casemanager'@'localhost';
 DROP ROLE IF EXISTS casemanager;
 CREATE ROLE 'casemanager';
 GRANT ALL ON susfund_db.* TO 'casemanager';
 CREATE USER 'casemanager'@'localhost' IDENTIFIED BY 'test1234';
 GRANT 'casemanager' TO 'casemanager'@'localhost';
 SET DEFAULT ROLE ALL TO 'casemanager'@'localhost';
-GRANT SUPER ON . TO 'casemanager'@'localhost';
+GRANT SUPER ON *.* TO 'casemanager'@'localhost';
 
 
 DROP SCHEMA IF EXISTS `susfund_db`;
