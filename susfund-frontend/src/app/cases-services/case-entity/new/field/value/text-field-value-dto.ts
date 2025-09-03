08d7c51866c605e66907ba6b2a9f82d1fec92948
Josefin Törner <josefintorner@gmail.com>
2025-09-03 14:53:30 +0200

    textfieldvalues frontend

--- susfund-frontend/src/app/cases-services/case-entity/new/field/value/text-field-value-dto.ts
+++ susfund-frontend/src/app/cases-services/case-entity/new/field/value/text-field-value-dto.ts
@@ -1,2 +1,8 @@
+import {FieldDefinitionDto} from '../definition/field-definition-dto';
+
 export interface TextFieldValueDto {
+  stringValue:string
+  owningCaseId:number,
+  owningFieldDefinition:FieldDefinitionDto,
+  dtoClass:string
 }
