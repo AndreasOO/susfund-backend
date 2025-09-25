a5a159af2bf1fac26340b10ce7606da11e2776e5
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-09-25 12:25:36 +0200

    Fixed wildfly crasching by moving back persistence.xml to repository module. Now both flyway and wildfly work at startup

--- susfund-flyway/src/main/java/org/andjos/flyway/base/JPAMigrationBase.java
+++ susfund-flyway/src/main/java/org/andjos/flyway/base/JPAMigrationBase.java
@@ -87,7 +87,6 @@ public abstract class JPAMigrationBase extends BaseJavaMigration {
         properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.format_sql", "true");
         properties.put("hibernate.connection.autocommit", "false");
-//        properties.put("hibernate.archive.autodetection", "class, hbm");
 
         // Create EntityManagerFactory
         return Persistence.createEntityManagerFactory("flyway-migration-pu", properties);
